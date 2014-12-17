137619a5e177bc383bfb88d6adea0752579e7407
wolfmc3@git.code.sf.net <marco.camplese.mc@gmail.com>
2014-12-18 00:45:27 +0100

    Created objects model

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='itmemory',
+    version='',
+    packages=['itmemory', 'customers', 'customers.migrations'],
+    url='',
+    license='',
+    author='marco',
+    author_email='',
+    description='', requires=['django']
+)
