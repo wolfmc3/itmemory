e6646059c2c4b47c61f90119bec981ed96ee2343
wolfmc3@git.code.sf.net <marco.camplese.mc@gmail.com>
2014-12-19 00:49:56 +0100

    Start implementation frontend

--- itmemory/urls.py
+++ itmemory/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from itmemory.views import Home
 
 urlpatterns = patterns('',
                        # Examples:
-                       # url(r'^$', 'itmemory.views.home', name='home'),
+                       url(r'^$', Home.as_view()),
                        # url(r'^blog/', include('blog.urls')),
-
+                       url(r'^objects/', include('objects.urls', namespace="objects")),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^grappelli/', include('grappelli.urls')),  # grappelli URLS
                        url(r'^admin_tools/', include('admin_tools.urls')),
+)
+
+urlpatterns += patterns(
+    'django.contrib.auth.views',
+    url(r'^login/$', 'login', {'template_name': 'login.html'}, name='mysite_login'),
+    url(r'^logout/$', 'logout', {'next_page': '/'}, name='mysite_logout'),
 )
--- objects/views.py
+++ objects/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views import generic
+from objects.models import HardwareObject, Settings
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'objects/index.html'
+    context_object_name = 'objects_list'
+
+    def get_queryset(self):
+        return HardwareObject.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = HardwareObject
+    template_name = 'objects/detail.html'
+    context_object_name = 'obj'
+
+    def get_queryset(self):
+        return HardwareObject.objects.all()
