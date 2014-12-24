04a183e7443c46e072de634c252c4e80e628b220
wolfmc3@git.code.sf.net <marco.camplese.mc@gmail.com>
2014-12-24 18:46:19 +0100

    Frontend (Tasks)

--- ittasks/views.py
+++ ittasks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views import generic
+from ittasks.models import Task
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'ittasks/index.html'
+    context_object_name = 'objects_list'
+
+    def get_queryset(self):
+        return Task.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Task
+    template_name = 'ittasks/detail.html'
+    context_object_name = 'obj'
+
+    def get_queryset(self):
+        return Task.objects.all()
