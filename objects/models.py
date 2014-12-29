f5eeb7dc6950cbbc6d12004fb5cbc4e17a260f1e
marco <marco.camplese.mc@gmail.com>
2014-12-29 21:10:09 +0100

    Password management for Hardware

--- objects/models.py
+++ objects/models.py
@@ -1,5 +1,9 @@
+# coding=utf-8
 from django.db import models
 from customers.models import WorkSite
+from simplecrypt import encrypt, decrypt
+from itmemory import settings as django_setting
+
 
 
 class HardwareObject(models.Model):
@@ -52,4 +56,24 @@ class Settings(models.Model):
     value = models.TextField(max_length=5000, verbose_name="Valore impostato")
 
     def __str__(self):
-        return str(self.hardwareobject) + " - " + self.type.name + ": " + self.value
+        return str(self.hardwareobject) + " - " + self.type.name + ": " + str(self.value)
+
+
+class SoftwarePassword(models.Model):
+    class Meta():
+        verbose_name = "Password"
+        verbose_name_plural = "Passwords"
+
+    hardwareobject = models.ForeignKey(HardwareObject, verbose_name="Oggetto")
+    settingtype = models.ForeignKey(SettingsType, related_name='softwarepasswords', verbose_name="Tipo impostazione")
+    url = models.URLField()
+    username = models.CharField(max_length=100, null=True, blank=True)
+    passwd = models.BinaryField(max_length=250, null=True, blank=True)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not str(self.passwd).startswith("sc\x00"):
+            self.passwd = encrypt(django_setting.SECRET_KEY, self.passwd).decode('string_escape')
+        super(SoftwarePassword, self).save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return str(self.hardwareobject) + " - " + self.settingtype.name + ": " + str(self.username)
