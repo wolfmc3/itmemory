05a9e2b1844cf8b2272751f0fbee9a1b98e32f0c
wolfmc3@git.code.sf.net <marco.camplese.mc@gmail.com>
2015-01-06 23:01:21 +0100

    requirements adjustment

--- pip-update.py
+++ pip-update.py
@@ -2,4 +2,4 @@ import pip
 from subprocess import call
 
 for dist in pip.get_installed_distributions():
-    call("pip-2.7 install --upgrade " + dist.project_name, shell=True)
+    call("pip install --upgrade " + dist.project_name, shell=True)
