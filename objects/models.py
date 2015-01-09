5bb4f77d15b6612a398d9d9658ccc1e4070ba39f
marco <marco.camplese.mc@gmail.com>
2015-01-09 12:31:05 +0100

    Admin adjustment

--- objects/models.py
+++ objects/models.py
@@ -20,7 +20,7 @@ class HardwareObject(models.Model):
     primary_ip = models.IPAddressField(default='0.0.0.0', verbose_name="Indirizzo IP primario")
     know_name = models.CharField(max_length=255, default='', verbose_name="Nome breve")
     image = models.ImageField(null=True, blank=True)
-    parentobject = models.ForeignKey('self',null=True,blank=True)
+    parentobject = models.ForeignKey('self',null=True,blank=True, verbose_name='Dipende da:')
 
     def _taskstodo(self):
         return self.tasks.filter(done=False, laststart__lte=datetime.now)
