a28e6b92ffadb9744b31061d427c1be0245c582e
wolfmc3@git.code.sf.net <marco.camplese.mc@gmail.com>
2015-01-12 01:26:48 +0100

    Template style optimization

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='itmemory',
+    version='',
+    packages=['lib', 'home', 'home.migrations', 'home.templatetags', 'ittasks', 'ittasks.migrations',
+              'ittasks.templatetags', 'objects', 'objects.migrations', 'objects.templatetags', 'itmemory', 'customers',
+              'customers.migrations'],
+    url='',
+    license='',
+    author='marco',
+    author_email='',
+    description=''
+)
