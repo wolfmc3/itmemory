2b1ba6448e47525eb0f95571e71d16327ec0d07b
wolfmc3@git.code.sf.net <marco.camplese.mc@gmail.com>
2015-01-18 23:50:54 +0100

    Module django_mailbox added

--- itmemory/settings.py
+++ itmemory/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django_mailbox',
     'jquery',
     'crispy_forms',
     'djfrontend',
