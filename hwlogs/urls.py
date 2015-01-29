7599b62be44525e153c4d8df1a8ccdfb3e54a017
marco <marco.camplese.mc@gmail.com>
2015-01-29 14:11:25 +0100

    Send Log over HTTP

--- hwlogs/urls.py
+++ hwlogs/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from hwlogs import views
-
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    url(r'^upload/$', views.UploadView.as_view(), name='upload'),
+    url(r'^upload/(?P<hwid>[0-9]+)/(?P<hwtoken>[\w\-]+)$', csrf_exempt(views.UploadView.as_view()), name='upload'),
 ]
--- hwlogs/views.py
+++ hwlogs/views.py
@@ -1,12 +1,22 @@
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
+from django.http.response import HttpResponse, HttpResponseNotFound
 from django.views import generic
+from hwlogs.models import LogsFromFile
+from django.shortcuts import get_object_or_404
+from objects.models import HardwareObject
 
 
 class UploadView(generic.TemplateView):
     template_name = 'hwlogs/upload.html'
 
     def post(self, request, *args, **kwargs):
-        fl = request.FILES['file_load']
-        path = default_storage.save('upload/' + fl.name, ContentFile(fl.read()))
-        return self.get(self, request, *args, **kwargs)
+        obj = get_object_or_404(HardwareObject,pk=kwargs.get("hwid",0))
+        if (obj.remote_token == kwargs.get("hwtoken","not provided")):
+            fl = request.FILES['file']
+            path = default_storage.save('upload/' + fl.name, ContentFile(fl.read()))
+            path = default_storage.base_location + "/" + path
+            LogsFromFile(path)
+            return HttpResponse("True")
+        else:
+            return HttpResponseNotFound('False')
